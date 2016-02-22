9bd71852351f29f0fe63348684da244e2f6da560
denis <klassik0110@gmail.com>
2016-02-22 19:46:50 +0300

    right test

--- src/test/java/sample/servlet/SampleServletApplicationTest.java
+++ src/test/java/sample/servlet/SampleServletApplicationTest.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 
 public class SampleServletApplicationTest {
 
-    SampleServletApplication sampleServletApplication = new SampleServletApplication();
+    private SampleServletApplication sampleServletApplication = new SampleServletApplication();
 
     @Test
     public void getGreeter() throws Exception {
         Assert.assertEquals("Hello World", sampleServletApplication.getGreeter(true));
-        Assert.assertEquals("There is no greeter for you", sampleServletApplication.getGreeter(true));
+        Assert.assertEquals("There is no greeter for you", sampleServletApplication.getGreeter(false));
     }
 }
